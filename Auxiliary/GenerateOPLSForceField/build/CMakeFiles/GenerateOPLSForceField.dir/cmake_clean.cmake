34771e6c70769f2f38cda44acecf2c7aa4f3121e
lr142 <lr-mail@163.com>
2022-12-03 15:14:19 +0800

    Major Version 2.0.0

---
+++ Auxiliary/GenerateOPLSForceField/build/CMakeFiles/GenerateOPLSForceField.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GenerateOPLSForceField.dir/forcefield.cpp.o"
+  "CMakeFiles/GenerateOPLSForceField.dir/main.cpp.o"
+  "CMakeFiles/GenerateOPLSForceField.dir/utility.cpp.o"
+  "GenerateOPLSForceField"
+  "GenerateOPLSForceField.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GenerateOPLSForceField.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Auxiliary/GenerateOPLSForceField/main.cpp
+++ Auxiliary/GenerateOPLSForceField/main.cpp
@@ -3,10 +3,15 @@
 #include "utility.h"
 using namespace std;
 
-int main() {
-    string PATH = "../";
+int main(int argc, char *argv[]) {
+    string PATH;
+    if (argc==1)
+        PATH = "../";
+    else
+        PATH = string(argv[1]);
+
     string OPLSAA_LT = PATH+"oplsaa.lt";
-    string atomic_weights_file = PATH+"../../Data/atomicweights.csv";
+    string atomic_weights_file = PATH+"atomicweights.csv";
     string atom_type_recognition_file = PATH+"AtomTypes_Input.txt";
     string output_file_name = PATH+"FFDescription_Output.txt";
     string functional_file = PATH+"functional.txt";
